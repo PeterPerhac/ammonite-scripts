dc456f8d5bae18334952c0df7b2998ac4dd7eb4b
Peter Perhac <perhac.peter@gmail.com>
2019-10-12 14:27:35 +0100

    adding my colorful picture generating ammonite script

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ammonite Scripts
+
+If I come up with any that I'd like to share, I'll add them to here.
+
---
+++ ppm/Colors.ppm.scala
@@ -0,0 +1,12 @@
+import ammonite.ops._
+
+@main
+def main(filename: String) = {
+  val outputFile = os.pwd/filename
+  write.over(outputFile, "P3\n256 256\n255\n")
+  for ((r,g) <- (0 to 255 by 1).zip(255 to 0 by -1)) {
+    val line = List.tabulate(256)(b => s"$r $g $b").mkString(" ")
+    write.append(outputFile, s"$line\n")
+  }
+}
+
